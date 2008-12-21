990901c66ab07047ff54e6f39ecbcba564ba6d20
sgolodetz <sgolodetz@gxstudios.net>
2008-12-21 14:54:56 +0000

    Added Vector3 and Constants.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@29 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/math/Constants.h
@@ -0,0 +1,17 @@
+/***
+ * hesperus: Constants.h
+ * Copyright Stuart Golodetz, 2008. All rights reserved.
+ ***/
+
+#ifndef H_HESP_MATH_CONSTANTS
+#define H_HESP_MATH_CONSTANTS
+
+namespace hesp {
+
+extern const double EPSILON;
+extern const double PI;
+extern const double SMALL_EPSILON;
+
+}
+
+#endif
---
+++ trunk/source/math/vectors/Vector3.h
@@ -0,0 +1,56 @@
+/***
+ * hesperus: Vector3.h
+ * Copyright Stuart Golodetz, 2008. All rights reserved.
+ ***/
+
+#ifndef H_HESP_MATH_VECTORS_VECTOR3D
+#define H_HESP_MATH_VECTORS_VECTOR3D
+
+#include <iostream>
+
+namespace hesp {
+
+template <typename T>
+struct Vector3
+{
+	//################## PUBLIC VARIABLES ##################//
+	T x, y, z;
+
+	//################## CONSTRUCTORS ##################//
+	Vector3();
+	Vector3(double x_, double y_, double z_);
+
+	//################## PUBLIC OPERATORS ##################//
+	Vector3& operator+=(const Vector3& rhs);
+	Vector3& operator-=(const Vector3& rhs);
+	Vector3& operator*=(double factor);
+	Vector3& operator/=(double factor);
+	Vector3 operator-() const;
+
+	//################## PUBLIC METHODS ##################//
+	Vector3 cross(const Vector3& rhs) const;
+	double distance(const Vector3& rhs) const;
+	double distance_squared(const Vector3& rhs) const;
+	double dot(const Vector3& rhs) const;
+	double length() const;
+	double length_squared() const;
+	Vector3& negate();
+	Vector3& normalize();
+	Vector3 project_onto(const Vector3& rhs);
+};
+
+//################## GLOBAL OPERATORS ##################//
+template <typename T> Vector3<T> operator+(const Vector3<T>& lhs, const Vector3<T>& rhs);
+template <typename T> Vector3<T> operator-(const Vector3<T>& lhs, const Vector3<T>& rhs);
+template <typename T> Vector3<T> operator*(double factor, const Vector3<T>& v);
+template <typename T> Vector3<T> operator*(const Vector3<T>& v, double factor);
+template <typename T> Vector3<T> operator/(const Vector3<T>& v, double factor);
+template <typename T> std::ostream& operator<<(std::ostream& os, const Vector3<T>& v);
+
+//################## TYPEDEFS ##################//
+typedef Vector3<float> Vector3f;
+typedef Vector3<double> Vector3d;
+
+}
+
+#endif
