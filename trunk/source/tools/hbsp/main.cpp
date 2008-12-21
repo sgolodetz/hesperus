4940f6007da8ec8280bc336442454bd2fb9f765a
sgolodetz <sgolodetz@gxstudios.net>
2008-12-21 13:44:06 +0000

    Started writing code to process the command line in hbsp.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@25 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/tools/hbsp/main.cpp
+++ trunk/source/tools/hbsp/main.cpp
@@ -3,7 +3,51 @@
  * Copyright Stuart Golodetz, 2008. All rights reserved.
  ***/
 
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <boost/lexical_cast.hpp>
+using boost::bad_lexical_cast;
+using boost::lexical_cast;
+
+enum GeomType
+{
+	COLLISION,
+	RENDERING
+};
+
+void quit_with_usage()
+{
+	std::cout << "Usage: hbsp {-r|-c} <input filename> <output filename> [-w<number>]" << std::endl;
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[])
 {
+	if(argc != 4 && argc != 5) quit_with_usage();
+
+	const std::vector<std::string> args(argv, argv+argc);
+
+	GeomType geomType;
+	if(args[1] == "-r") geomType = RENDERING;
+	else if(args[1] == "-c") geomType = COLLISION;
+	else quit_with_usage();
+
+	std::string inputFilename = args[2];
+	std::string outputFilename = args[3];
+
+	double weight = 4;
+	if(argc == 5)
+	{
+		if(args[4].substr(0,2) != "-w") quit_with_usage();
+
+		try							{ weight = lexical_cast<double,std::string>(args[4].substr(2)); }
+		catch(bad_lexical_cast&)	{ quit_with_usage(); }
+	}
+
+	// TODO
+
 	return 0;
 }
