5261d73956ef27e6b3ea02b6c4e95b0a018d8b1e
sgolodetz <sgolodetz@gxstudios.net>
2008-12-22 16:05:59 +0000

    Changed BSPTree to output the number of nodes in the tree when writing to a file.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@46 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/bsp/BSPTree.cpp
+++ trunk/source/bsp/BSPTree.cpp
@@ -5,6 +5,8 @@
 
 #include "BSPTree.h"
 
+#include <ostream>
+
 namespace hesp {
 
 //#################### CONSTRUCTORS ####################
@@ -15,6 +17,7 @@ BSPTree::BSPTree(const std::vector<BSPNode_Ptr>& nodes)
 //#################### PUBLIC METHODS ####################
 void BSPTree::output_postorder_text(std::ostream& os) const
 {
+	os << m_nodes.size() << '\n';
 	root()->output_postorder_text(os);
 }
 
