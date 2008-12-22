098e55e145465378ff20f02e6b0d334de217b626
sgolodetz <sgolodetz@gxstudios.net>
2008-12-22 14:26:35 +0000

    Moved VertBuilder into its own header in order to tidy up the main code in hbsp.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@39 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/tools/hbsp/main.cpp
+++ trunk/source/tools/hbsp/main.cpp
@@ -22,6 +22,7 @@ using boost::lexical_cast;
 #include <source/math/vectors/Vector3.h>
 #include "QuitFunctions.h"
 #include "RenderingVector3d.h"
+#include "VertBuilder.h"
 using namespace hesp;
 
 //#################### ENUMERATIONS ####################
@@ -32,56 +33,6 @@ enum GeomType
 };
 
 //#################### CLASSES ####################
-template <typename Vert> struct VertBuilder;
-
-template <> struct VertBuilder<Vector3d>
-{
-	static Vector3d build_vertex(const std::vector<std::string>& components)
-	{
-		if(components.size() != 3) quit_with_error("Incorrect number of vertex components");
-
-		try
-		{
-			double x = lexical_cast<double,std::string>(components[0]);
-			double y = lexical_cast<double,std::string>(components[1]);
-			double z = lexical_cast<double,std::string>(components[2]);
-			return Vector3d(x,y,z);
-		}
-		catch(bad_lexical_cast&)
-		{
-			quit_with_error("One of the vertex components is not a number");
-
-			// This will never happen.
-			throw 23;
-		}
-	}
-};
-
-template <> struct VertBuilder<RenderingVector3d>
-{
-	static RenderingVector3d build_vertex(const std::vector<std::string>& components)
-	{
-		if(components.size() != 5) quit_with_error("Incorrect number of vertex components");
-
-		try
-		{
-			double x = lexical_cast<double,std::string>(components[0]);
-			double y = lexical_cast<double,std::string>(components[1]);
-			double z = lexical_cast<double,std::string>(components[2]);
-			double u = lexical_cast<double,std::string>(components[3]);
-			double v = lexical_cast<double,std::string>(components[4]);
-			return RenderingVector3d(x,y,z,u,v);
-		}
-		catch(bad_lexical_cast&)
-		{
-			quit_with_error("One of the vertex components is not a number");
-
-			// This will never happen.
-			throw 23;
-		}
-	}
-};
-
 struct PolyIndex
 {
 	int index;
