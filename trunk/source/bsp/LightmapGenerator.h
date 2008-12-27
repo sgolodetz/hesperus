d43c5b3a14202f632aff372ef8ad75b0214bbd7d
sgolodetz <sgolodetz@gxstudios.net>
2008-12-27 21:30:06 +0000

    Started work on LightmapGenerator.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@106 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/bsp/LightmapGenerator.h
@@ -0,0 +1,19 @@
+/***
+ * hesperus: LightmapGenerator.h
+ * Copyright Stuart Golodetz, 2008. All rights reserved.
+ ***/
+
+#ifndef H_HESP_BSP_LIGHTMAPGENERATOR
+#define H_HESP_BSP_LIGHTMAPGENERATOR
+
+namespace hesp {
+
+template <typename Vert, typename AuxData>
+class LightmapGenerator
+{
+	// TODO
+};
+
+}
+
+#endif
