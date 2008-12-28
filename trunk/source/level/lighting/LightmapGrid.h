1831d8134b0cdf4cefea4bda36cd259814740d2f
sgolodetz <sgolodetz@gxstudios.net>
2008-12-28 19:42:39 +0000

    Finished designing the interface for LightmapGrid.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@124 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/lighting/LightmapGrid.h
+++ trunk/source/level/lighting/LightmapGrid.h
@@ -6,19 +6,32 @@
 #ifndef H_HESP_LEVEL_LIGHTING_LIGHTMAPGRID
 #define H_HESP_LEVEL_LIGHTING_LIGHTMAPGRID
 
+#include <vector>
+
 #include <boost/shared_ptr.hpp>
 using boost::shared_ptr;
 
+#include <source/level/bsp/BSPTree.h>
 #include <source/math/geom/Polygon.h>
 #include <source/math/vectors/TexCoords.h>
+#include "Light.h"
+#include "Lightmap.h"
 
 namespace hesp {
 
 class LightmapGrid
 {
+	//#################### NESTED CLASSES ####################
+private:
+	struct GridPoint
+	{
+		Vector3d position;		// where does the grid point lie in world space?
+		bool withinPolygon;		// is it within the polygon being lightmapped? (if not, we can skip some work when constructing a lightmap)
+	};
+
 	//#################### PRIVATE VARIABLES ####################
 private:
-	// TODO
+	std::vector<std::vector<GridPoint> > m_grid;
 
 	//#################### STATIC FACTORY METHODS ####################
 public:
@@ -27,7 +40,7 @@ public:
 
 	//#################### PUBLIC METHODS ####################
 public:
-	// TODO
+	Lightmap_Ptr lightmap_from_light(const Light& light, const BSPTree_Ptr& tree) const;
 };
 
 //#################### TYPEDEFS ####################
