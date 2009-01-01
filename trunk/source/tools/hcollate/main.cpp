c335930ecdd76b392ae6f5520fc1c812d7c441a9
sgolodetz <sgolodetz@gxstudios.net>
2009-01-01 23:40:47 +0000

    Wrote a bit of code for hcollate.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@174 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/tools/hcollate/main.cpp
+++ trunk/source/tools/hcollate/main.cpp
@@ -3,7 +3,34 @@
  * Copyright Stuart Golodetz, 2009. All rights reserved.
  ***/
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+//#################### FUNCTIONS ####################
+void quit_with_error(const std::string& error)
+{
+	std::cout << "Error: " << error << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+void quit_with_usage()
+{
+	std::cout << "Usage: hcollate <input lit tree> <input portals> <input vis> <output filename>" << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+void collate(const std::string& treeFilename, const std::string& portalsFilename, const std::string& visFilename, const std::string& outputFilename)
+{
+	
+
+	// TODO
+}
+
 int main(int argc, char *argv[])
 {
+	if(argc != 5) quit_with_usage();
+	std::vector<std::string> args(argv, argv + argc);
+	collate(args[0], args[1], args[2], args[3]);
 	return 0;
 }
