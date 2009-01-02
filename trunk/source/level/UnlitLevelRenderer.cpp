99f25fcfc3f364d6b8298696caecb2d765bbd277
sgolodetz <sgolodetz@gxstudios.net>
2009-01-02 22:18:41 +0000

    Updated UnlitLevelRenderer::render_simple() to render textured polygons.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@196 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/UnlitLevelRenderer.cpp
+++ trunk/source/level/UnlitLevelRenderer.cpp
@@ -46,22 +46,27 @@ void UnlitLevelRenderer::render(const std::vector<int>& polyIndices) const
 //#################### PRIVATE METHODS ####################
 void UnlitLevelRenderer::render_simple(const std::vector<int>& polyIndices) const
 {
-	glPushAttrib(GL_POLYGON_BIT);
+	glPushAttrib(GL_ENABLE_BIT | GL_POLYGON_BIT);
 
-	Colour3d colours[] = {Colour3d(1,0,0), Colour3d(0,1,0), Colour3d(0,0,1)};
+	glEnable(GL_TEXTURE_2D);
+	glColor3d(1,1,1);
 
 	int indexCount = static_cast<int>(polyIndices.size());
 	for(int i=0; i<indexCount; ++i)
 	{
 		TexturedPolygon_Ptr poly = m_polygons[polyIndices[i]];
+
+		// Note:	If we got to this point, all textures were loaded successfully,
+		//			so the texture's definitely in the map.
+		std::map<std::string,Texture_Ptr>::const_iterator jt = m_textures.find(poly->auxiliary_data());
+		jt->second->bind();
+
 		int vertCount = poly->vertex_count();
 		glBegin(GL_POLYGON);
 			for(int j=0; j<vertCount; ++j)
 			{
-				int k = (i+j)%3;
-				glColor3d(colours[k].r, colours[k].g, colours[k].b);
-
 				const TexturedVector3d& v = poly->vertex(j);
+				glTexCoord2d(v.u, v.v);
 				glVertex3d(v.x, v.y, v.z);
 			}
 		glEnd();
