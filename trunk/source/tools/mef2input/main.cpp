8eb9abd244bebe232f2c0247eed5b5c6a0511a0b
sgolodetz <sgolodetz@gxstudios.net>
2009-01-03 16:31:32 +0000

    mef2input: Wrote the code to parse the command line.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@207 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/tools/mef2input/main.cpp
+++ trunk/source/tools/mef2input/main.cpp
@@ -3,7 +3,32 @@
  * Copyright Stuart Golodetz, 2009. All rights reserved.
  ***/
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+//#################### FUNCTIONS ####################
+void quit_with_error(const std::string& error)
+{
+	std::cout << "Error: " << error << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+void quit_with_usage()
+{
+	std::cout << "Usage: mef2input <input MEF> <output brushes> <output entities> <output lights>" << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+void run_converter(const std::string& inputFilename, const std::string& brushesFilename, const std::string& entitiesFilename, const std::string& lightsFilename)
+{
+	// TODO
+}
+
 int main(int argc, char *argv[])
 {
+	if(argc != 5) quit_with_usage();
+	std::vector<std::string> args(argv, argv + argc);
+	run_converter(args[1], args[2], args[3], args[4]);
 	return 0;
 }
