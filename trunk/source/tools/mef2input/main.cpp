3c3d1d0c35e00c1678d9f45f40803d20e5faeff8
sgolodetz <sgolodetz@gxstudios.net>
2009-01-03 17:26:08 +0000

    mef2input: Implemented a first version of read_polyhedral_brush().
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@210 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/tools/mef2input/main.cpp
+++ trunk/source/tools/mef2input/main.cpp
@@ -8,6 +8,10 @@
 #include <string>
 #include <vector>
 
+#include <boost/lexical_cast.hpp>
+using boost::bad_lexical_cast;
+using boost::lexical_cast;
+
 #include <source/exceptions/Exception.h>
 using namespace hesp;
 
@@ -22,6 +26,11 @@ void quit_with_usage()
 {
 	std::cout << "Usage: mef2input <input MEF> <output brushes> <output entities> <output lights>" << std::endl;
 	exit(EXIT_FAILURE);
+}
+
+void read_line(std::istream& is, std::string& line, const std::string& attemptedAction)
+{
+	if(!std::getline(is, line)) throw Exception("Unexpected EOF whilst trying to " + attemptedAction);
 }
 
 void skip_section(std::istream& is)
@@ -39,18 +48,42 @@ void skip_section(std::istream& is)
 
 void read_polyhedral_brush(std::istream& is)
 {
-	skip_section(is);
+	std::string line;
+
+	read_line(is, line, "read PolyhedralBrush");
+	if(line != "{") throw Exception("PolyhedralBrush: Expected {");
+
+	read_line(is, line, "read bounds");
+	if(line.substr(0,6) != "Bounds") throw Exception("PolyhedralBrush: Expected Bounds");
+
+	read_line(is, line, "read polygon count");
+	if(line.substr(0,9) != "PolyCount" || line.length() < 11) throw Exception("PolyhedralBrush: Expected PolyCount");
+
+	int polyCount;
+	try							{ polyCount = lexical_cast<int,std::string>(line.substr(10)); }
+	catch(bad_lexical_cast&)	{ throw Exception("PolyhedralBrush: Polygon count is not an integer"); }
+
+	for(int i=0; i<polyCount; ++i)
+	{
+		read_line(is, line, "read polygon");
+		if(line.substr(0,7) != "Polygon" || line.length() < 9) throw Exception("PolyhedralBrush: Expected Polygon");
+
+		// TODO: Parse polygon.
+	}
+
+	read_line(is, line, "read PolyhedralBrush");
+	if(line != "}") throw Exception("PolyhedralBrush: Expected }");
 }
 
 void read_architecture_brush_composite(std::istream& is)
 {
 	std::string line;
-	if(!std::getline(is, line)) throw Exception("Unexpected EOF whilst trying to read ArchitectureBrushComposite");
+	read_line(is, line, "read ArchitectureBrushComposite");
 	if(line != "{") throw Exception("ArchitectureBrushComposite: Expected {");
 
 	for(;;)
 	{
-		if(!std::getline(is, line)) throw Exception("Unexpected EOF whilst trying to read ArchitectureBrushComposite");
+		read_line(is, line, "read ArchitectureBrushComposite");
 		if(line == "}") break;
 
 		if(line == "ArchitectureBrushComposite") read_architecture_brush_composite(is);
@@ -71,10 +104,10 @@ void run_converter(const std::string& inputFilename, const std::string& brushesF
 	// Read in the MEF file.
 	std::string line;
 
-	if(!std::getline(is, line)) throw Exception("Unexpected EOF whilst trying to read MEF ID");
+	read_line(is, line, "read MEF ID");
 	if(line != "MEF 2") throw Exception("Bad MEF ID or unexpected file version");
 
-	if(!std::getline(is, line)) throw Exception("Unexpected EOF whilst trying to read Textures section");
+	read_line(is, line, "read Textures");
 	if(line != "Textures") throw Exception("Textures section is missing");
 	skip_section(is);
 
@@ -88,6 +121,15 @@ void run_converter(const std::string& inputFilename, const std::string& brushesF
 			skip_section(is);
 		}
 	}
+
+	// Write the brushes to disk.
+	// TODO
+
+	// Write the entities to disk.
+	// TODO
+
+	// Write the lights to disk.
+	// TODO
 }
 
 int main(int argc, char *argv[])
