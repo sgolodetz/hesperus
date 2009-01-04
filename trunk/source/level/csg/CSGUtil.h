5c9c9b02eb94df5d12d187ff38e913a8e42fdbb8
sgolodetz <sgolodetz@gxstudios.net>
2009-01-04 15:09:16 +0000

    Started work on CSGUtil.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@224 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/level/csg/CSGUtil.h
@@ -0,0 +1,20 @@
+/***
+ * hesperus: CSGUtil.h
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#ifndef H_HESP_LEVEL_CSG_CSGUTIL
+#define H_HESP_LEVEL_CSG_CSGUTIL
+
+namespace hesp {
+
+class CSGUtil
+{
+	// TODO
+};
+
+}
+
+#include "CSGUtil.tpp"
+
+#endif
