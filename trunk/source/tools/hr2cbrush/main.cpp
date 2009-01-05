421dbf4afa405149423d0b67b8dde2cf1994742d
sgolodetz <sgolodetz@gxstudios.net>
2009-01-05 18:30:37 +0000

    Did a bit of work on hr2cbrush.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@246 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/tools/hr2cbrush/main.cpp
+++ trunk/source/tools/hr2cbrush/main.cpp
@@ -3,7 +3,32 @@
  * Copyright Stuart Golodetz, 2009. All rights reserved.
  ***/
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+//#################### FUNCTIONS ####################
+void quit_with_error(const std::string& error)
+{
+	std::cout << "Error: " << error << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+void quit_with_usage()
+{
+	std::cout << "Usage: hr2cbrush <input filename> <output filename>" << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+void run_converter(const std::string& inputFilename, const std::string& outputFilename)
+{
+	// TODO
+}
+
 int main(int argc, char *argv[])
 {
+	if(argc != 3) quit_with_usage();
+	std::vector<std::string> args(argv, argv + argc);
+	run_converter(args[1], args[2]);
 	return 0;
 }
