88729f1bf6e52b17540e4ddead6e2542a074341b
sgolodetz <sgolodetz@gxstudios.net>
2009-01-07 17:04:17 +0000

    Updated the level-making batch file and added a new bounding box to AABBS.txt (for testing purposes).
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@275 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/bin/resources/levels/make.bat
+++ trunk/bin/resources/levels/make.bat
@@ -17,7 +17,6 @@ hbsp -r %1.rg2 %1.rt2
 hportal -r %1.rt2 %1.rp2
 hvis %1.rp2 %1.vis
 hlight %1.rt2 %1.vis %1.lum LM %1.lbt
-hcollate +L %1.lbt %1.rp2 %1.vis %1.bsp
 
 REM Collision Section
 
@@ -30,6 +29,22 @@ hbsp -c %%~nf.cg1 %%~nf.ct1
 hportal -c %%~nf.ct1 %%~nf.cp1
 hflood -c %%~nf.ct1 %%~nf.cp1 %1.ent %%~nf.cg2
 )
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+set CG2FILES=
+FOR %%f IN (*.cg2) DO (
+set CG2FILES=!CG2FILES! %%f
+)
+echo hobsp %CG2FILES% %1.obt
+set CG2FILES=
+ENDLOCAL
+
+echo hoportal %1.obt %1.opr
+echo hnav %1.obt %1.opr %1.nav
+
+REM Collation Section (this will need updating ultimately)
+
+hcollate +L %1.lbt %1.rp2 %1.vis %1.bsp
 
 REM Cleanup Rendering Section
 
