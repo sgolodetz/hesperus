d657abf4d82fa98efbe335d0d4c2d21dda8f4c3d
sgolodetz <sgolodetz@gxstudios.net>
2009-01-07 15:57:41 +0000

    Updated the level-making batch file and copied the AABBs settings file across to be used by hexpand.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@272 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/bin/resources/levels/make.bat
+++ trunk/bin/resources/levels/make.bat
@@ -6,23 +6,38 @@ cd %1
 set PATH=%PATH%;..\..\..\tools
 
 mef2input %1.mef %1.rbr %1.ent %1.lum
+
+REM Rendering Section
+
 hcsg -r %1.rbr %1.rg1
 hbsp -r %1.rg1 %1.rt1
-hportal -r %1.rt1 %1.pr1
-hflood -r %1.rt1 %1.pr1 %1.ent %1.rg2
+hportal -r %1.rt1 %1.rp1
+hflood -r %1.rt1 %1.rp1 %1.ent %1.rg2
 hbsp -r %1.rg2 %1.rt2
-hportal -r %1.rt2 %1.pr2
-hvis %1.pr2 %1.vis
+hportal -r %1.rt2 %1.rp2
+hvis %1.rp2 %1.vis
 hlight %1.rt2 %1.vis %1.lum LM %1.lbt
-hcollate +L %1.lbt %1.pr2 %1.vis %1.bsp
+hcollate +L %1.lbt %1.rp2 %1.vis %1.bsp
+
+REM Collision Section
+
+hr2cbrush %1.rbr %1.cbr
+hexpand ..\..\settings\AABBs.txt %1.cbr
+
+REM Cleanup Rendering Section
 
 del *.rbr
 del *.rg*
 del *.rt*
-del *.pr*
+del *.rp*
 del *.vis
 del *.lbt
 del *.bmp
+
+REM Cleanup Collision Section
+
+del *.cbr
+del *.ebr
 
 cd ..
 
