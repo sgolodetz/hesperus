8652a3e7a95f03a3b66a343608362d2ccafa119c
sgolodetz <sgolodetz@gxstudios.net>
2009-01-07 21:29:44 +0000

    Designed the interface for OnionCompiler.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@283 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/onionbsp/OnionCompiler.h
+++ trunk/source/level/onionbsp/OnionCompiler.h
@@ -6,9 +6,70 @@
 #ifndef H_HESP_LEVEL_ONIONBSP_ONIONCOMPILER
 #define H_HESP_LEVEL_ONIONBSP_ONIONCOMPILER
 
+#include <map>
+#include <set>
+#include <vector>
+
+#include <source/math/geom/Plane.h>
+#include "OnionPlane.h"
+#include "OnionTree.h"
+
 namespace hesp {
 
-// TODO
+template <typename Poly>
+class OnionCompiler
+{
+	//#################### TYPEDEFS ####################
+private:
+	typedef shared_ptr<Poly> Poly_Ptr;
+	typedef std::vector<Poly_Ptr> PolyVector;
+	typedef shared_ptr<PolyVector> PolyVector_Ptr;
+	typedef std::set<OnionPlane_Ptr> OnionPlaneSet;
+
+	//#################### NESTED CLASSES ####################
+private:
+	struct PolyIndex
+	{
+		int index;
+		bool splitCandidate;	// is the onion plane of the referenced polygon a split candidate?
+
+		PolyIndex(int index_, bool splitCandidate_)
+		:	index(index_), splitCandidate(splitCandidate_)
+		{}
+	};
+
+	//#################### PRIVATE VARIABLES ####################
+private:
+	// Input data
+	double m_weight;
+
+	// Intermediate data
+	OnionPlaneSet m_onionPlanes;
+	std::map<int,int> m_polyToOnionPlane;
+
+	// Intermediate / output data
+	PolyVector_Ptr m_polygons;
+
+	// Output data
+	OnionTree_Ptr m_tree;
+
+	//#################### CONSTRUCTORS ####################
+public:
+	OnionCompiler(const std::vector<PolyVector_Ptr>& maps, double weight);
+
+	//#################### PUBLIC METHODS ####################
+public:
+	void build_tree();
+	const PolyVector_Ptr& polygons() const;
+	const OnionTree_Ptr& tree() const;
+
+	//#################### PRIVATE METHODS ####################
+private:
+	OnionNode_Ptr build_subtree(const std::vector<PolyIndex>& polyIndices, std::vector<OnionNode_Ptr>& nodes,
+								const std::vector<PlaneClassifier>& relativeToClosestAncestor);
+	OnionPlane_Ptr choose_split_plane(const std::vector<PolyIndex>& polyIndices);
+	void determine_onion_planes(const std::vector<PolyVector_Ptr>& maps);
+};
 
 }
 
--- trunk/source/tools/hobsp/main.cpp
+++ trunk/source/tools/hobsp/main.cpp
@@ -3,6 +3,9 @@
  * Copyright Stuart Golodetz, 2009. All rights reserved.
  ***/
 
+#include <source/level/onionbsp/OnionCompiler.h>
+using namespace hesp;
+
 int main(int argc, char *argv[])
 {
 	return 0;
