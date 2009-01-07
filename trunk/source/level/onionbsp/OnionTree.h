7f68bfd41f0567ae9c76d6d0d12d668146358cf0
sgolodetz <sgolodetz@gxstudios.net>
2009-01-07 19:35:53 +0000

    Designed the interface for OnionTree itself.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@281 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/onionbsp/OnionTree.h
+++ trunk/source/level/onionbsp/OnionTree.h
@@ -6,11 +6,40 @@
 #ifndef H_HESP_LEVEL_ONIONBSP_ONIONTREE
 #define H_HESP_LEVEL_ONIONBSP_ONIONTREE
 
+#include <source/math/vectors/Vector3.h>
+#include "OnionLeaf.h"
+
 namespace hesp {
+
+//#################### TYPEDEFS ####################
+typedef shared_ptr<class OnionTree> OnionTree_Ptr;
+typedef shared_ptr<const class OnionTree> OnionTree_CPtr;
 
 class OnionTree
 {
-	// TODO
+	//#################### PRIVATE VARIABLES ####################
+private:
+	std::vector<OnionNode_Ptr> m_nodes;
+	std::vector<OnionLeaf*> m_leaves;
+
+	//#################### CONSTRUCTORS ####################
+public:
+	OnionTree(const std::vector<OnionNode_Ptr>& nodes);
+
+	//#################### PUBLIC METHODS ####################
+public:
+	int find_leaf_index(const Vector3d& p) const;
+	const OnionLeaf *leaf(int n) const;
+	bool line_of_sight(int mapIndex, const Vector3d& p1, const Vector3d& p2) const;
+	static OnionTree_Ptr load_postorder_text(std::istream& is);
+	void output_postorder_text(std::ostream& os) const;
+	OnionNode_Ptr root() const;
+
+	//#################### PRIVATE METHODS ####################
+private:
+	void index_leaves();
+	void index_leaves_sub(const OnionNode_Ptr& node);
+	bool line_of_sight_sub(int mapIndex, const Vector3d& p1, const Vector3d& p2, const OnionNode_Ptr& node) const;
 };
 
 }
