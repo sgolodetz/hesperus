49dc14a1e7d3b3582312cc8e4c028efb1da9416f
sgolodetz <sgolodetz@gxstudios.net>
2009-01-11 16:53:57 +0000

    Tidied up FileUtil by removing all the commented-out code.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@341 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/io/FileUtil.cpp
+++ trunk/source/io/FileUtil.cpp
@@ -6,43 +6,16 @@
 #include "FileUtil.h"
 
 #include <fstream>
-#include <sstream>
 
 #include <boost/lexical_cast.hpp>
-#include <boost/tokenizer.hpp>
 using boost::bad_lexical_cast;
 using boost::lexical_cast;
 
 #include <source/exceptions/Exception.h>
-#include <source/images/BitmapLoader.h>
-#include <source/images/BitmapSaver.h>
-#include <source/level/LitLevelRenderer.h>
-#include <source/level/UnlitLevelRenderer.h>
 
 namespace hesp {
 
 //#################### PUBLIC METHODS ####################
-#if 0
-/**
-Loads a level from the specified file.
-
-@param filename	The name of the level file
-@return			The level
-*/
-Level_Ptr FileUtil::load_level_file(const std::string& filename)
-{
-	std::ifstream is(filename.c_str(), std::ios_base::binary);
-	if(is.fail()) throw Exception("Could not open " + filename + " for reading");
-
-	std::string fileType;
-	if(!std::getline(is, fileType)) throw Exception("Unexpected EOF whilst trying to read file type");
-
-	if(fileType == "HBSPL") return load_lit_level_file(is);
-	else if(fileType == "HBSPU") return load_unlit_level_file(is);
-	else throw Exception(filename + " is not a valid level file");
-}
-#endif
-
 /**
 Loads an empty leaf count and an array of portals from the specified file.
 
@@ -63,106 +36,7 @@ void FileUtil::load_portals_file(const std::string& filename, int& emptyLeafCoun
 	FileSectionUtil::load_polygons_section(is, "Portals", portals);
 }
 
-#if 0
-/**
-Saves all the relevant pieces of information to the specified level file.
-
-@param filename		The name of the output file
-@param polygons		The level polygons
-@param tree			The BSP tree for the level
-@param portals		The portals for the level
-@param leafVis		The leaf visibility table for the level
-*/
-void FileUtil::save_level_file(const std::string& filename, const std::vector<TexturedPolygon_Ptr>& polygons,
-							   const BSPTree_Ptr& tree, const std::vector<Portal_Ptr>& portals,
-							   const LeafVisTable_Ptr& leafVis)
-{
-	std::ofstream os(filename.c_str(), std::ios_base::binary);
-	if(os.fail()) throw Exception("Could not open " + filename + " for writing");
-
-	os << "HBSPU\n";
-	FileSectionUtil::save_polygons_section(os, "Polygons", polygons);
-	os << "***\n";
-	FileSectionUtil::save_tree_section(os, tree);
-	os << "***\n";
-	FileSectionUtil::save_polygons_section(os, "Portals", portals);
-	os << "***\n";
-	FileSectionUtil::save_vis_section(os, leafVis);
-}
-
-/**
-Saves all the relevant pieces of information to the specified level file.
-
-@param filename		The name of the output file
-@param polygons		The level polygons
-@param tree			The BSP tree for the level
-@param portals		The portals for the level
-@param leafVis		The leaf visibility table for the level
-@param lightmaps	The lightmaps for the level
-*/
-void FileUtil::save_level_file(const std::string& filename, const std::vector<TexturedLitPolygon_Ptr>& polygons,
-							   const BSPTree_Ptr& tree, const std::vector<Portal_Ptr>& portals,
-							   const LeafVisTable_Ptr& leafVis, const std::vector<Image24_Ptr>& lightmaps)
-{
-	std::ofstream os(filename.c_str(), std::ios_base::binary);
-	if(os.fail()) throw Exception("Could not open " + filename + " for writing");
-
-	os << "HBSPL\n";
-	FileSectionUtil::save_polygons_section(os, "Polygons", polygons);
-	os << "***\n";
-	FileSectionUtil::save_tree_section(os, tree);
-	os << "***\n";
-	FileSectionUtil::save_polygons_section(os, "Portals", portals);
-	os << "***\n";
-	FileSectionUtil::save_vis_section(os, leafVis);
-	os << "***\n";
-
-	int lightmapCount = static_cast<int>(lightmaps.size());
-	for(int i=0; i<lightmapCount; ++i)
-	{
-		BitmapSaver::save_image24(os, lightmaps[i]);
-	}
-}
-#endif
-
 //#################### PRIVATE METHODS ####################
-#if 0
-/**
-Loads a lit level from the specified std::istream.
-
-@param is	The std::istream
-@return		The lit level
-*/
-Level_Ptr FileUtil::load_lit_level_file(std::istream& is)
-{
-	// Load the data.
-	std::vector<TexturedLitPolygon_Ptr> polygons;
-	BSPTree_Ptr tree;
-	std::vector<Portal_Ptr> portals;
-	LeafVisTable_Ptr leafVis;
-
-	FileSectionUtil::load_polygons_section(is, "Polygons", polygons);
-	load_separator(is);
-	tree = FileSectionUtil::load_tree_section(is);
-	load_separator(is);
-	FileSectionUtil::load_polygons_section(is, "Portals", portals);
-	load_separator(is);
-	leafVis = FileSectionUtil::load_vis_section(is);
-	load_separator(is);
-
-	int polyCount = static_cast<int>(polygons.size());
-	std::vector<Image24_Ptr> lightmaps(polyCount);
-	for(int i=0; i<polyCount; ++i)
-	{
-		lightmaps[i] = BitmapLoader::load_image24(is);
-	}
-
-	// Construct and return the level.
-	LevelRenderer_Ptr levelRenderer(new LitLevelRenderer(polygons, lightmaps));
-	return Level_Ptr(new Level(levelRenderer, tree, portals, leafVis));
-}
-#endif
-
 /**
 Loads a separator (a line containing only ***) from the specified std::istream.
 
@@ -174,34 +48,5 @@ void FileUtil::load_separator(std::istream& is)
 	if(!std::getline(is, line)) throw Exception("Unexpected EOF whilst trying to read separator");
 	if(line != "***") throw Exception("Bad separator");
 }
-
-#if 0
-/**
-Loads an unlit level from the specified std::istream.
-
-@param is	The std::istream
-@return		The unlit level
-*/
-Level_Ptr FileUtil::load_unlit_level_file(std::istream& is)
-{
-	// Load the data.
-	std::vector<TexturedPolygon_Ptr> polygons;
-	BSPTree_Ptr tree;
-	std::vector<Portal_Ptr> portals;
-	LeafVisTable_Ptr leafVis;
-
-	FileSectionUtil::load_polygons_section(is, "Polygons", polygons);
-	load_separator(is);
-	tree = FileSectionUtil::load_tree_section(is);
-	load_separator(is);
-	FileSectionUtil::load_polygons_section(is, "Portals", portals);
-	load_separator(is);
-	leafVis = FileSectionUtil::load_vis_section(is);
-
-	// Construct and return the level.
-	LevelRenderer_Ptr levelRenderer(new UnlitLevelRenderer(polygons));
-	return Level_Ptr(new Level(levelRenderer, tree, portals, leafVis));
-}
-#endif
 
 }
--- trunk/source/io/FileUtil.h
+++ trunk/source/io/FileUtil.h
@@ -11,13 +11,10 @@
 #include <boost/shared_ptr.hpp>
 using boost::shared_ptr;
 
-#include <source/images/Image.h>
-#include <source/level/Level.h>
 #include <source/level/brushes/PolyhedralBrush.h>
 #include <source/level/bsp/BSPTree.h>
 #include <source/level/onionbsp/OnionTree.h>
 #include <source/level/portals/Portal.h>
-#include <source/util/PolygonTypes.h>
 
 namespace hesp {
 
@@ -26,9 +23,6 @@ class FileUtil
 	//#################### LOADING METHODS ####################
 public:
 	template <typename Poly> static std::vector<shared_ptr<PolyhedralBrush<Poly> > > load_brushes_file(const std::string& filename);
-#if 0
-	static Level_Ptr load_level_file(const std::string& filename);
-#endif
 	template <typename Poly> static void load_onion_tree_file(const std::string& filename, std::vector<shared_ptr<Poly> >& polygons, OnionTree_Ptr& tree);
 	template <typename Poly> static void load_polygons_file(const std::string& filename, std::vector<shared_ptr<Poly> >& polygons);
 	static void load_portals_file(const std::string& filename, int& emptyLeafCount, std::vector<Portal_Ptr>& portals);
@@ -37,24 +31,10 @@ public:
 	//#################### SAVING METHODS ####################
 public:
 	template <typename Poly> static void save_brushes_file(const std::string& filename, const std::vector<shared_ptr<PolyhedralBrush<Poly> > >& brushes);
-#if 0
-	static void save_level_file(const std::string& filename, const std::vector<TexturedPolygon_Ptr>& polygons,
-								const BSPTree_Ptr& tree, const std::vector<Portal_Ptr>& portals,
-								const LeafVisTable_Ptr& leafVis);
-	static void save_level_file(const std::string& filename, const std::vector<TexturedLitPolygon_Ptr>& polygons,
-								const BSPTree_Ptr& tree, const std::vector<Portal_Ptr>& portals,
-								const LeafVisTable_Ptr& leafVis, const std::vector<Image24_Ptr>& lightmaps);
-#endif
 
 	//#################### LOADING SUPPORT METHODS ####################
 private:
-#if 0
-	static Level_Ptr load_lit_level_file(std::istream& is);
-#endif
 	static void load_separator(std::istream& is);
-#if 0
-	static Level_Ptr load_unlit_level_file(std::istream& is);
-#endif
 };
 
 }
