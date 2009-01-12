b24624d18626607b676ba497ecdb2f3ce74b30dd
sgolodetz <sgolodetz@gxstudios.net>
2009-01-12 22:00:54 +0000

    hnav: Wrote the trivial code to parse the command line.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@353 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/tools/hnav/main.cpp
+++ trunk/source/tools/hnav/main.cpp
@@ -3,7 +3,37 @@
  * Copyright Stuart Golodetz, 2009. All rights reserved.
  ***/
 
-int main(int argc, char *argv[])
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <source/exceptions/Exception.h>
+using namespace hesp;
+
+//#################### FUNCTIONS ####################
+void quit_with_error(const std::string& error)
 {
+	std::cout << "Error: " << error << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+void quit_with_usage()
+{
+	std::cout << "Usage: hnav <input onion tree> <output navmesh>" << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+void run_generator(const std::string& inputFilename, const std::string& outputFilename)
+{
+	// TODO
+}
+
+int main(int argc, char *argv[])
+try
+{
+	if(argc != 3) quit_with_usage();
+	std::vector<std::string> args(argv, argv + argc);
+	run_generator(args[1], args[2]);
 	return 0;
 }
+catch(Exception& e) { quit_with_error(e.cause()); }
