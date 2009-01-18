448fccf513530a84acadeb98da551b5c7ec884c2
sgolodetz <sgolodetz@gxstudios.net>
2009-01-18 20:40:20 +0000

    Started work on GlobalPathfinder.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@413 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/level/nav/GlobalPathfinder.cpp
@@ -0,0 +1,12 @@
+/***
+ * hesperus: GlobalPathfinder.cpp
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#include "GlobalPathfinder.h"
+
+namespace hesp {
+
+// TODO
+
+}
---
+++ trunk/source/level/nav/GlobalPathfinder.h
@@ -0,0 +1,18 @@
+/***
+ * hesperus: GlobalPathfinder.h
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#ifndef H_HESP_LEVEL_NAV_GLOBALPATHFINDER
+#define H_HESP_LEVEL_NAV_GLOBALPATHFINDER
+
+namespace hesp {
+
+class GlobalPathfinder
+{
+	// TODO
+};
+
+}
+
+#endif
