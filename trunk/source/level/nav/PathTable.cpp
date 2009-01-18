631ee9cdd901c5d66cc3a9a37bb87694c9213ee4
sgolodetz <sgolodetz@gxstudios.net>
2009-01-18 14:47:07 +0000

    Renamed PathTable.tpp to PathTable.cpp.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@401 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/nav/PathTable.cpp
+++ trunk/source/level/nav/PathTable.cpp
@@ -1,7 +1,9 @@
 /***
- * hesperus: PathTable.tpp
+ * hesperus: PathTable.cpp
  * Copyright Stuart Golodetz, 2009. All rights reserved.
  ***/
+
+#include "PathTable.h"
 
 namespace hesp {
 
--- trunk/source/level/nav/PathTable.h
+++ trunk/source/level/nav/PathTable.h
@@ -12,6 +12,4 @@ namespace hesp {
 
 }
 
-#include "PathTable.tpp"
-
 #endif
