8a08abf1f0269bc96a3f059aeb66e9685a520272
sgolodetz <sgolodetz@gxstudios.net>
2009-01-21 16:02:19 +0000

    Started work on EntityManager.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@444 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/level/entities/EntityManager.h
@@ -0,0 +1,18 @@
+/***
+ * hesperus: EntityManager.h
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#ifndef H_HESP_ENTITYMANAGER
+#define H_HESP_ENTITYMANAGER
+
+namespace hesp {
+
+class EntityManager
+{
+	// TODO
+};
+
+}
+
+#endif
