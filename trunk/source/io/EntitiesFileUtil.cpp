388ade5ae647951bf0c9c2afc9d4d69a966a775c
sgolodetz <sgolodetz@gxstudios.net>
2009-01-21 14:25:55 +0000

    Started work on EntitiesFileUtil.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@439 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/io/EntitiesFileUtil.cpp
@@ -0,0 +1,12 @@
+/***
+ * hesperus: EntitiesFileUtil.cpp
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#include "EntitiesFileUtil.h"
+
+namespace hesp {
+
+// TODO
+
+}
---
+++ trunk/source/io/EntitiesFileUtil.h
@@ -0,0 +1,19 @@
+/***
+ * hesperus: EntitiesFileUtil.h
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#ifndef H_HESP_ENTITIESFILEUTIL
+#define H_HESP_ENTITIESFILEUTIL
+
+namespace hesp {
+
+struct EntitiesFileUtil
+{
+	//#################### LOADING METHODS ####################
+	// TODO
+};
+
+}
+
+#endif
