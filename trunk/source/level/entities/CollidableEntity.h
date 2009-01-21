e4dd4d0ca4a59d7274fe3bc677fed98bee7e13be
sgolodetz <sgolodetz@gxstudios.net>
2009-01-21 11:52:54 +0000

    Implemented CollidableEntity.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@435 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/entities/CollidableEntity.h
+++ trunk/source/level/entities/CollidableEntity.h
@@ -6,6 +6,9 @@
 #ifndef H_HESP_COLLIDABLEENTITY
 #define H_HESP_COLLIDABLEENTITY
 
+#include <vector>
+
+#include <source/math/geom/AABB.h>
 #include "LocatableEntity.h"
 
 namespace hesp {
@@ -16,7 +19,22 @@ with this entity, so it maintains appropriate collision AABBs).
 */
 class CollidableEntity : virtual public LocatableEntity
 {
-	// TODO
+	//#################### PROTECTED VARIABLES ####################
+protected:
+	std::vector<int> m_aabbIndices;
+
+	//#################### CONSTRUCTORS ####################
+public:
+	CollidableEntity(const Vector3d& position, const std::vector<int>& aabbIndices)
+	:	LocatableEntity(position), m_aabbIndices(aabbIndices)
+	{}
+
+	//#################### PUBLIC METHODS ####################
+public:
+	const std::vector<int>& aabb_indices() const
+	{
+		return m_aabbIndices;
+	}
 };
 
 }
