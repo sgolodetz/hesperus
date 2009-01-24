b54c351a04af5b63b404ce1baa38fd61ca1fea43
sgolodetz <sgolodetz@gxstudios.net>
2009-01-24 13:43:03 +0000

    Updated hcollate to accept the entities file as an extra command-line parameter, and modified the level-making batch file to pass it in correctly.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@472 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/tools/hcollate/main.cpp
+++ trunk/source/tools/hcollate/main.cpp
@@ -33,13 +33,13 @@ void quit_with_error(const std::string& error)
 
 void quit_with_usage()
 {
-	std::cout << "Usage: hcollate {+L <input lit tree> | -L <input tree>} <input portals> <input vis> <input onion tree> <input onion portals> <input nav data> <output filename>" << std::endl;
+	std::cout << "Usage: hcollate {+L <input lit tree> | -L <input tree>} <input portals> <input vis> <input onion tree> <input onion portals> <input nav data> <input entities> <output filename>" << std::endl;
 	exit(EXIT_FAILURE);
 }
 
 void collate_lit(const std::string& treeFilename, const std::string& portalsFilename, const std::string& visFilename,
 				 const std::string& onionTreeFilename, const std::string& onionPortalsFilename,
-				 const std::string& navFilename, const std::string& outputFilename)
+				 const std::string& navFilename, const std::string& entitiesFilename, const std::string& outputFilename)
 try
 {
 	// Load the lit polygons, tree and lightmap prefix.
@@ -94,7 +94,7 @@ catch(Exception& e) { quit_with_error(e.cause()); }
 
 void collate_unlit(const std::string& treeFilename, const std::string& portalsFilename, const std::string& visFilename,
 				   const std::string& onionTreeFilename, const std::string& onionPortalsFilename,
-				   const std::string& navFilename, const std::string& outputFilename)
+				   const std::string& navFilename, const std::string& entitiesFilename, const std::string& outputFilename)
 try
 {
 	// Load the unlit polygons and tree.
@@ -138,11 +138,11 @@ catch(Exception& e) { quit_with_error(e.cause()); }
 
 int main(int argc, char *argv[])
 {
-	if(argc != 9) quit_with_usage();
+	if(argc != 10) quit_with_usage();
 	std::vector<std::string> args(argv, argv + argc);
 
-	if(args[1] == "+L") collate_lit(args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
-	else if(args[1] == "-L") collate_unlit(args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
+	if(args[1] == "+L") collate_lit(args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
+	else if(args[1] == "-L") collate_unlit(args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
 	else quit_with_usage();
 
 	return 0;
