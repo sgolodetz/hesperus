0c7d61ae0ae34dd6e4cbdd80c94dd6a69bf1f03c
sgolodetz <sgolodetz@gxstudios.net>
2009-01-25 18:52:14 +0000

    Started work on the Game class.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@487 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/game/Game.cpp
@@ -0,0 +1,12 @@
+/***
+ * hesperus: Game.cpp
+ * Copyright Stuart Golodetz, 2008. All rights reserved.
+ ***/
+
+#include "Game.h"
+
+namespace hesp {
+
+// TODO
+
+}
---
+++ trunk/source/game/Game.h
@@ -0,0 +1,18 @@
+/***
+ * hesperus: Game.h
+ * Copyright Stuart Golodetz, 2008. All rights reserved.
+ ***/
+
+#ifndef H_HESP_GAME
+#define H_HESP_GAME
+
+namespace hesp {
+
+class Game
+{
+	// TODO
+};
+
+}
+
+#endif
