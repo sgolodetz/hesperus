340dcc9be357ddf2460c5c02b4cff1ea02b68bff
sgolodetz <sgolodetz@gxstudios.net>
2009-01-27 22:25:54 +0000

    Started work on UserBipedYoke.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@509 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/yokes/user/UserBipedYoke.cpp
@@ -0,0 +1,12 @@
+/***
+ * hesperus: UserBipedYoke.cpp
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#include "UserBipedYoke.h"
+
+namespace hesp {
+
+// TODO
+
+}
---
+++ trunk/source/yokes/user/UserBipedYoke.h
@@ -0,0 +1,18 @@
+/***
+ * hesperus: UserBipedYoke.h
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#ifndef H_HESP_USERBIPEDYOKE
+#define H_HESP_USERBIPEDYOKE
+
+namespace hesp {
+
+class UserBipedYoke
+{
+	// TODO
+};
+
+}
+
+#endif
