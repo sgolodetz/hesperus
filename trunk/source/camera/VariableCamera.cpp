9b608361ac5be3ce21f40165cfd1c98199b35ced
sgolodetz <sgolodetz@gxstudios.net>
2009-01-28 20:26:07 +0000

    Got full 3D mouse look working.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@540 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/camera/VariableCamera.cpp
+++ trunk/source/camera/VariableCamera.cpp
@@ -8,6 +8,7 @@
 #include <source/ogl/WrappedGL.h>
 #include <gl/glu.h>
 
+#include <source/math/Constants.h>
 #include <source/math/vectors/VectorUtil.h>
 
 namespace hesp {
@@ -57,12 +58,32 @@ const Vector3d& VariableCamera::position() const
 
 void VariableCamera::rotate(const Vector3d& axis, double angle)
 {
-	// Note: We try and optimise things a little by observing that there's no point rotating
-	// an axis about itself and that generally when we rotate about an axis, we'll be passing
-	// it in as the parameter axis, e.g. camera.rotate(camera.n(), Math.PI/2).
-	if(&axis != &m_nVector) m_nVector = rotate_about_axis(m_nVector, angle, axis);
+	// Note 1:	We try and optimise things a little by observing that there's no point rotating an
+	//			axis about itself and that generally when we rotate about an axis, we'll be passing
+	//			it in as the parameter axis, e.g. camera.rotate(camera.n(), Math.PI/2).
+
+	// Note 2:	We prevent the camera's new n vector from being directly upwards or downwards.
+
+	// Note 3:	We prevent the camera's new v vector from pointing below the horizontal.
+
+	Vector3d newN = m_nVector;
+	if(&axis != &m_nVector)
+	{
+		newN = rotate_about_axis(m_nVector, angle, axis);
+		if(newN.cross(Vector3d(0,0,1)).length() < EPSILON) return;
+	}
+
+	Vector3d newV = m_vVector;
+	if(&axis != &m_vVector)
+	{
+		newV = rotate_about_axis(m_vVector, angle, axis);
+		if(newV.z < 0) return;
+	}
+
+	m_nVector = newN;
+	m_vVector = newV;
+
 	if(&axis != &m_uVector) m_uVector = rotate_about_axis(m_uVector, angle, axis);
-	if(&axis != &m_vVector) m_vVector = rotate_about_axis(m_vVector, angle, axis);
 }
 
 void VariableCamera::set_position(const Vector3d& position)
--- trunk/source/level/entities/BipedTurnCommand.cpp
+++ trunk/source/level/entities/BipedTurnCommand.cpp
@@ -17,12 +17,14 @@ void BipedTurnCommand::execute(int milliseconds)
 {
 	Camera& camera = m_biped->camera_component()->camera();
 
-	const double CAMERA_SPEED = 15.0;
+	const double CAMERA_SPEED = 20.0;
 
 	// For left-right motion, rotate about the (0,0,1) axis.
-#if 1
 	camera.rotate(Vector3d(0,0,1), -m_mouseMotionX * CAMERA_SPEED * (milliseconds/1000.0));
-#endif
+
+	// For up-down motion, rotate about the camera's u axis. Note that this rotation is clamped internally by the camera
+	// to prevent the n vector being parallel to (0,0,1).
+	camera.rotate(camera.u(), m_mouseMotionY * CAMERA_SPEED * (milliseconds/1000.0));
 }
 
 }
