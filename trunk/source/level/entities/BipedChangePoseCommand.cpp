8ebb6dd6030224894dc375ebe33a43d7abec4169
sgolodetz <sgolodetz@gxstudios.net>
2009-01-31 07:48:23 +0000

    Added a couple of notes to BipedChangePoseCommand::execute().
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@560 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/entities/BipedChangePoseCommand.cpp
+++ trunk/source/level/entities/BipedChangePoseCommand.cpp
@@ -15,6 +15,8 @@ BipedChangePoseCommand::BipedChangePoseCommand(const Entity_Ptr& biped)
 //#################### PUBLIC METHODS ####################
 void BipedChangePoseCommand::execute(const std::vector<AABB3d>& aabbs, const OnionTree_Ptr& tree, int milliseconds)
 {
+	// FIXME: Crouching is currently a "jolt" from one pose to another. It should really be a smooth transition.
+
 	ICameraComponent_Ptr camComponent = m_biped->camera_component();
 	ICollisionComponent_Ptr colComponent = m_biped->collision_component();
 
@@ -30,12 +32,13 @@ void BipedChangePoseCommand::execute(const std::vector<AABB3d>& aabbs, const Oni
 
 	Vector3d dest = source + Vector3d(0,0,deltaZ);
 
+	// Check that changing pose won't put us in a wall.
 	OnionTree::Transition transition = tree->find_first_transition(curPose, source, dest);
 	if(transition.classifier != OnionTree::RAY_EMPTY) return;
-
 	transition = tree->find_first_transition(newPose, source, dest);
 	if(transition.classifier != OnionTree::RAY_EMPTY) return;
 
+	// If the pose change is ok, set the new pose and update the player position to reflect the centre of the new AABB.
 	colComponent->set_pose(newPose);
 	camComponent->camera().set_position(dest);
 }
