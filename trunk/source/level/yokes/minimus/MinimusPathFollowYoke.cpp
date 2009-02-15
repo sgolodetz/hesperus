48ba1f483ce518b4b0803cbf2f44dc8f95a559e9
sgolodetz <sgolodetz@gxstudios.net>
2009-02-15 22:11:06 +0000

    Changed the constructor of MinimusPathFollowYoke to check the biped constraint.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@657 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/yokes/minimus/MinimusPathFollowYoke.cpp
+++ trunk/source/level/yokes/minimus/MinimusPathFollowYoke.cpp
@@ -10,7 +10,15 @@ namespace hesp {
 //#################### CONSTRUCTORS ####################
 MinimusPathFollowYoke::MinimusPathFollowYoke(const Entity_Ptr& biped)
 :	m_biped(biped)
-{}
+{
+	if(!m_biped->camera_component() ||
+	   !m_biped->collision_component() ||
+	   !m_biped->nav_component() ||
+	   !m_biped->physics_component())
+	{
+		throw Exception("Couldn't attach a biped yoke to a non-biped");
+	}
+}
 
 //#################### PUBLIC METHODS ####################
 std::vector<EntityCommand_Ptr> MinimusPathFollowYoke::generate_commands(UserInput& input)
