6b7ac9bbf783aa24968142f23f5117b141287626
sgolodetz <sgolodetz@gxstudios.net>
2009-02-26 01:23:05 +0000

    Started work on the models package.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@692 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/level/models/Bone.h
@@ -0,0 +1,18 @@
+/***
+ * hesperus: Bone.h
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#ifndef H_HESP_BONE
+#define H_HESP_BONE
+
+namespace hesp {
+
+class Bone
+{
+	// TODO
+};
+
+}
+
+#endif
---
+++ trunk/source/level/models/Model.h
@@ -0,0 +1,18 @@
+/***
+ * hesperus: Model.h
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#ifndef H_HESP_MODEL
+#define H_HESP_MODEL
+
+namespace hesp {
+
+class Model
+{
+	// TODO
+};
+
+}
+
+#endif
