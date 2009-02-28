441d24ecd36b07d1294b7d48020edbddf877ce45
sgolodetz <sgolodetz@gxstudios.net>
2009-02-28 13:04:01 +0000

    Started work on Matrix44.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@699 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/math/matrices/Matrix44.cpp
@@ -0,0 +1,12 @@
+/***
+ * hesperus: Matrix44.cpp
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#include "Matrix44.h"
+
+namespace hesp {
+
+// TODO
+
+}
---
+++ trunk/source/math/matrices/Matrix44.h
@@ -0,0 +1,18 @@
+/***
+ * hesperus: Matrix44.h
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#ifndef H_HESP_MATRIX44
+#define H_HESP_MATRIX44
+
+namespace hesp {
+
+class Matrix44
+{
+	// TODO
+};
+
+}
+
+#endif
