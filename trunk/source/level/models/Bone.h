c795f66b791deed97ee8b3bddb33a5c99e719db0
sgolodetz <sgolodetz@gxstudios.net>
2009-02-28 13:10:42 +0000

    Continued working on Bone, and added a note containing the code for some Visual Studio macros.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@700 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/models/Bone.h
+++ trunk/source/level/models/Bone.h
@@ -6,10 +6,29 @@
 #ifndef H_HESP_BONE
 #define H_HESP_BONE
 
+#include <string>
+
+#include <boost/shared_ptr.hpp>
+using boost::shared_ptr;
+
+#include <source/math/matrices/Matrix44.h>
+
 namespace hesp {
+
+//#################### TYPEDEFS ####################
+typedef shared_ptr<class Bone> Bone_Ptr;
+typedef shared_ptr<const class Bone> Bone_CPtr;
 
 class Bone
 {
+	//#################### PRIVATE VARIABLES ####################
+private:
+	int m_id;
+	std::string m_name;
+	Matrix44 m_absoluteMatrix;
+	Matrix44 m_relativeMatrix;
+	Bone_Ptr m_parent;
+
 	// TODO
 };
 
