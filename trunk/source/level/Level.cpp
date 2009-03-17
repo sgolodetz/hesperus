b8ff6809758d9fd681b4aa6c904d18ddb17776cd
sgolodetz <sgolodetz@gxstudios.net>
2009-03-17 19:38:39 +0000

    Sorted out some code which was unnecessarily setting matrix elements to 0 twice.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@753 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/Level.cpp
+++ trunk/source/level/Level.cpp
@@ -170,10 +170,10 @@ void Level::render_entities() const
 			//			orientation.
 			Matrix44_Ptr mat = Matrix44::zeros();
 			Matrix44& m = *mat;
-			m(0,0) = u.x;	m(0,1) = -n.x;	m(0,2) = 0;		m(0,3) = p.x;
-			m(1,0) = u.y;	m(1,1) = -n.y;	m(1,2) = 0;		m(1,3) = p.y;
-			m(2,0) = u.z;	m(2,1) = -n.z;	m(2,2) = 1;		m(2,3) = p.z;
-			m(3,0) = 0;		m(3,1) = 0;		m(3,2) = 0;		m(3,3) = 1;
+			m(0,0) = u.x;		m(0,1) = -n.x;		/*m(0,2) = 0;*/		m(0,3) = p.x;
+			m(1,0) = u.y;		m(1,1) = -n.y;		/*m(1,2) = 0;*/		m(1,3) = p.y;
+			m(2,0) = u.z;		m(2,1) = -n.z;		m(2,2) = 1;			m(2,3) = p.z;
+			/*m(3,0) = 0;*/		/*m(3,1) = 0;*/		/*m(3,2) = 0;*/		m(3,3) = 1;
 
 			glPushMatrix();
 			glMultMatrixd(m.rep());
