5ee3671318907da82a56bce8d6fb1920d865d8fd
sgolodetz <sgolodetz@gxstudios.net>
2009-03-18 14:01:16 +0000

    Started work on Quaternion.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@757 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/math/quaternions/Quaternion.cpp
@@ -0,0 +1,12 @@
+/***
+ * hesperus: Quaternion.cpp
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#include "Quaternion.h"
+
+namespace hesp {
+
+// TODO
+
+}
---
+++ trunk/source/math/quaternions/Quaternion.h
@@ -0,0 +1,18 @@
+/***
+ * hesperus: Quaternion.h
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#ifndef H_HESP_QUATERNION
+#define H_HESP_QUATERNION
+
+namespace hesp {
+
+class Quaternion
+{
+	// TODO
+};
+
+}
+
+#endif
