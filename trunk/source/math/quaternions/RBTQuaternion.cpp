3d73a40895f6c472eb964d4aa4d83e21cc0ed1e0
sgolodetz <sgolodetz@gxstudios.net>
2009-03-20 22:40:42 +0000

    Started work on RBTQuaternion.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@762 b5f66140-8697-46b1-94f4-efb67e06acc7

---
+++ trunk/source/math/quaternions/RBTQuaternion.cpp
@@ -0,0 +1,12 @@
+/***
+ * hesperus: RBTQuaternion.cpp
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#include "RBTQuaternion.h"
+
+namespace hesp {
+
+// TODO
+
+}
---
+++ trunk/source/math/quaternions/RBTQuaternion.h
@@ -0,0 +1,18 @@
+/***
+ * hesperus: RBTQuaternion.h
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#ifndef H_HESP_RBTQUATERNION
+#define H_HESP_RBTQUATERNION
+
+namespace hesp {
+
+class RBTQuaternion
+{
+	// TODO
+};
+
+}
+
+#endif
