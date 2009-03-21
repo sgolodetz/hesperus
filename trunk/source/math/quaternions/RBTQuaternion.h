b4553b611dcf9b6ceace448cddb618214c8fb44c
sgolodetz <sgolodetz@gxstudios.net>
2009-03-21 13:59:37 +0000

    Started designing the interface for RBTQuaternion.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@765 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/math/quaternions/RBTQuaternion.h
+++ trunk/source/math/quaternions/RBTQuaternion.h
@@ -13,6 +13,10 @@ using boost::shared_ptr;
 
 namespace hesp {
 
+//#################### TYPEDEFS ####################
+typedef shared_ptr<class RBTQuaternion> RBTQuaternion_Ptr;
+typedef shared_ptr<const class RBTQuaternion> RBTQuaternion_CPtr;
+
 /**
 This class represents a rigid-body transform as a quaternion and translation vector.
 */
@@ -20,15 +24,21 @@ class RBTQuaternion
 {
 	//#################### PRIVATE VARIABLES ####################
 private:
-	Quaternion m_rot;
-	Vector3d m_trans;
+	Quaternion m_rotation;
+	Vector3d m_translation;
 
-	// TODO
+	//#################### CONSTRUCTORS ####################
+private:
+	RBTQuaternion();
+
+	//#################### STATIC FACTORY METHODS ####################
+public:
+	static RBTQuaternion_Ptr from_axis_angle_translation(Vector3d axis, double angle, const Vector3d& translation);
+
+	//#################### PUBLIC METHODS ####################
+public:
+	Vector3d apply(const Vector3d& p) const;
 };
-
-//#################### TYPEDEFS ####################
-typedef shared_ptr<RBTQuaternion> RBTQuaternion_Ptr;
-typedef shared_ptr<const RBTQuaternion> RBTQuaternion_CPtr;
 
 }
 
