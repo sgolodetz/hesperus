48e0f8cd349810226e8ad9bc543fee9ff055a266
sgolodetz <sgolodetz@gxstudios.net>
2009-03-24 18:23:46 +0000

    Switched to using PNGs for the game textures.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@800 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/GeometryRenderer.cpp
+++ trunk/source/level/GeometryRenderer.cpp
@@ -5,7 +5,7 @@
 
 #include "GeometryRenderer.h"
 
-#include <source/images/BitmapLoader.h>
+#include <source/images/PNGLoader.h>
 #include <source/io/util/DirectoryFinder.h>
 #include <source/textures/TextureFactory.h>
 namespace bf = boost::filesystem;
@@ -19,8 +19,8 @@ void GeometryRenderer::load_textures(const std::set<std::string>& textureNames)
 
 	for(std::set<std::string>::const_iterator it=textureNames.begin(), iend=textureNames.end(); it!=iend; ++it)
 	{
-		std::string filename = (texturesDir / (*it + ".bmp")).file_string();
-		Image24_Ptr image = BitmapLoader::load_image24(filename);
+		std::string filename = (texturesDir / (*it + ".png")).file_string();
+		Image24_Ptr image = PNGLoader::load_image24(filename);
 		m_textures.insert(std::make_pair(*it, TextureFactory::create_texture24(image)));
 	}
 }
