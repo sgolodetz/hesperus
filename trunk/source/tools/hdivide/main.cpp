c3c60db43df3b4f1b7c2dbdc6e98af1fb56daf5a
sgolodetz <sgolodetz@gxstudios.net>
2009-04-07 17:27:48 +0000

    Did a bit of work on hdivide.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@839 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/tools/hdivide/main.cpp
+++ trunk/source/tools/hdivide/main.cpp
@@ -3,8 +3,39 @@
  * Copyright Stuart Golodetz, 2009. All rights reserved.
  ***/
 
-int main(int argc, char *argv[])
+#include <iostream>
+#include <string>
+#include <vector>
+
+//#################### FUNCTIONS ####################
+void quit_with_error(const std::string& error)
+{
+	std::cout << "Error: " << error << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+void quit_with_usage()
+{
+	std::cout << "Usage: hdivide <input brushes> <output normal brushes> <output collision brushes> <output detail polygons> <output hint planes> <output special brushes>" << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+void run_divider(const std::string& inputBrushesFilename, const std::string& normalBrushesFilename,
+				 const std::string& collisionBrushesFilename, const std::string& detailPolygonsFilename,
+				 const std::string& hintPlanesFilename, const std::string& specialBrushesFilename)
 {
 	// TODO
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc != 7) quit_with_usage();
+	std::vector<std::string> args(argv, argv + argc);
+
+	// Validate arguments.
+	// TODO
+
+	run_divider(args[1], args[2], args[3], args[4], args[5], args[6]);
+
 	return 0;
 }
