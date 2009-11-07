94cf5ec804494fa31821ebc3af90dca29f867461
sgolodetz <sgolodetz@gxstudios.net>
2009-11-07 12:37:40 +0000

    Implemented CmpProjectileWeaponUsable::use().
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus@1038 b5f66140-8697-46b1-94f4-efb67e06acc7

--- trunk/source/level/objects/components/CmpProjectileWeaponUsable.cpp
+++ trunk/source/level/objects/components/CmpProjectileWeaponUsable.cpp
@@ -5,10 +5,7 @@
 
 #include "CmpProjectileWeaponUsable.h"
 
-#if 0
-// TEMPORARY
-#include <iostream>
-#endif
+#include <SDL.h>
 
 #include <source/util/Properties.h>
 
@@ -44,21 +41,33 @@ Properties CmpProjectileWeaponUsable::save() const
 
 void CmpProjectileWeaponUsable::use()
 {
-#if 0
-	// TEMPORARY
-	boost::optional<Vector3d> pos = hotspot_position("root");
-	boost::optional<Vector3d> ori = hotspot_orientation("root");
-	if(pos && ori)
-	{
-		std::cout << "Creating bullet at position " << *pos << " with orientation " << *ori << std::endl;
-	}
-	else
-	{
-		std::cout << "Could not create bullet: hotspot 'root' not initialised yet" << std::endl;
-	}
-#endif	
+	unsigned long curTime = SDL_GetTicks();
 
-	// TODO
+	// If the weapon's never been fired, or it's been at least the specified firing interval since it was fired,
+	// or the time's wrapped around because we've been playing too long (> 49 days!), then fire the weapon.
+	// Note that strictly speaking we should check that the appropriate amount of time has elapsed even if we
+	// wrapped round, but it only happens after 49 days of continuous playing, so I'm not going to devote lots
+	// of attention to it - if someone fires one bullet earlier than they should strictly be able to, it won't
+	// ruin the game.
+	if(!m_lastFired || curTime >= *m_lastFired + static_cast<unsigned long>(m_firingInterval) || curTime < *m_lastFired)
+	{
+		// Fire a bullet from each hotspot of the weapon (note that this in principle makes it easy to implement things like double-barrelled shotguns).
+		const std::vector<std::string>& spots = hotspots();
+		for(size_t i=0, size=spots.size(); i<size; ++i)
+		{
+			boost::optional<Vector3d> pos = hotspot_position(spots[i]);
+			boost::optional<Vector3d> ori = hotspot_orientation(spots[i]);
+			if(pos && ori)
+			{
+				ObjectSpecification specification = m_objectManager->get_archetype(m_projectileType);
+				specification.set_component_property("Simulation", "Position", *pos);
+				specification.set_component_property("Simulation", "Velocity", m_muzzleSpeed * *ori);
+				m_objectManager->queue_for_construction(specification);
+			}
+		}
+
+		m_lastFired = curTime;
+	}
 }
 
 }
--- trunk/source/level/objects/components/CmpProjectileWeaponUsable.h
+++ trunk/source/level/objects/components/CmpProjectileWeaponUsable.h
@@ -6,6 +6,8 @@
 #ifndef H_HESP_CMPPROJECTILEWEAPONUSABLE
 #define H_HESP_CMPPROJECTILEWEAPONUSABLE
 
+#include <boost/optional.hpp>
+
 #include "CmpUsable.h"
 
 namespace hesp {
@@ -14,7 +16,8 @@ class CmpProjectileWeaponUsable : public CmpUsable
 {
 	//#################### PRIVATE VARIABLES ####################
 private:
-	int m_firingInterval;	// the interval between firing one shot and the next (in milliseconds)
+	int m_firingInterval;							// the interval between firing one shot and the next (in milliseconds)
+	boost::optional<unsigned long> m_lastFired;		// the time at which the weapon was last fired
 	double m_muzzleSpeed;
 	std::string m_projectileType;
 
